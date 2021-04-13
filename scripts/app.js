8a43956bb5bd174c07597d9925e35e13c60a73c8
max-b10 <bungaymax@gmail.com>
2021-04-13 21:34:34 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 "use strict";
+
+const startButton = document.querySelector(".start-button");
 const grid = document.querySelector(".grid");
 const gridWidth = 332;
 const gridHeight = 332;
@@ -13,10 +15,15 @@ const ballStartPosition = [157.5, 59];
 let ballPosition = ballStartPosition;
 
 // Set these to determine direction of ball when game starts (pixels):
-let ballXDirection = -0.5;
-let ballYDirection = 0.5;
+let ballXDirection = 0;
+let ballYDirection = 0;
 
 let ballMovementInterval;
+
+startButton.addEventListener("click", () => {
+  startGame();
+  startButton.style.display = "none";
+});
 
 // Brick class:
 class Brick {
@@ -62,62 +69,3 @@ placePlatform();
 grid.appendChild(platform);
 
 document.addEventListener("keydown", arrowControls);
-
-// Create and append the ball:
-const ball = document.createElement("div");
-ball.classList.add("ball");
-placeBall();
-grid.appendChild(ball);
-
-function ballMovement() {
-  ballPosition[0] += ballXDirection;
-  ballPosition[1] += ballYDirection;
-  placeBall();
-  ifBallBounces();
-}
-// The lower the interval, the quicker the ball.
-ballMovementInterval = setInterval(ballMovement, 1);
-
-function ifBallBounces() {
-  // If ball hits a wall:
-  if (
-    // top wall:
-    ballPosition[1] >= gridHeight - ballSize ||
-    // left wall:
-    ballPosition[0] <= 0 ||
-    // right wall:
-    ballPosition[0] >= gridWidth - ballSize
-  ) {
-    changeDirection();
-  }
-  // If ball hits the platform:
-  if (
-    ballPosition[0] > currentPosition[0] &&
-    ballPosition[0] < currentPosition[0] + brickWidth &&
-    ballPosition[1] > currentPosition[1] &&
-    ballPosition[1] < currentPosition[1] + brickHeight
-  ) {
-    // console.log(ballPosition);
-    changeDirection();
-  }
-  // If ball hits a brick from the bricks array:
-  for (let i = 0; i < bricks.length; i++) {
-    if (
-      ballPosition[0] > bricks[i].bottomLeft[0] &&
-      ballPosition[0] < bricks[i].bottomRight[0] &&
-      ballPosition[1] + ballSize > bricks[i].bottomLeft[1] &&
-      ballPosition[1] < bricks[i].topLeft[1]
-    ) {
-      const allBricks = Array.from(document.querySelectorAll(".brick"));
-      allBricks[i].classList.remove("brick");
-      bricks.splice(i, 1);
-      changeDirection();
-    }
-  }
-
-  // If ball reaches bottom of grid i.e. game over:
-  if (ballPosition[1] <= 0) {
-    clearInterval(ballMovementInterval);
-    document.removeEventListener("keydown", arrowControls);
-  }
-}
