1bed2f8b6f2f0e7e82e545e2027841ff79a2f8a8
max-b10 <bungaymax@gmail.com>
2021-04-13 21:34:12 +0100

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -16,11 +16,84 @@ function placePlatform() {
   platform.style.left = `${startingPosition[0]}px`;
   platform.style.bottom = `${startingPosition[1]}px`;
 }
+// Start game button function:
+function startGame() {
+  document.addEventListener("keydown", arrowControls);
+  // Create and append the ball:
+  const ball = document.createElement("div");
+  ball.classList.add("ball");
+  // Determine position of the ball:
+  function placeBall() {
+    ball.style.left = `${ballPosition[0]}px`;
+    ball.style.bottom = `${ballPosition[1]}px`;
+  }
+  grid.appendChild(ball);
 
-// Dtermine position of the ball:
-function placeBall() {
-  ball.style.left = `${ballPosition[0]}px`;
-  ball.style.bottom = `${ballPosition[1]}px`;
+  placeBall();
+
+  // Starting directon of the ball:
+  ballXDirection = 0.5;
+  ballYDirection = 0.5;
+
+  function ballMovement() {
+    ballPosition[0] += ballXDirection;
+    ballPosition[1] += ballYDirection;
+    placeBall();
+    ifBallBounces();
+  }
+
+  // The lower the interval, the quicker the ball moves.
+  ballMovementInterval = setInterval(ballMovement, 1);
+
+  function ifBallBounces() {
+    // If ball hits a wall:
+    if (
+      // top wall:
+      ballPosition[1] >= gridHeight - ballSize ||
+      // left wall:
+      ballPosition[0] <= 0 ||
+      // right wall:
+      ballPosition[0] >= gridWidth - ballSize
+    ) {
+      console.log(ballXDirection, ballYDirection);
+
+      changeDirection();
+    }
+    // If ball hits the platform:
+    if (
+      ballPosition[0] > currentPosition[0] &&
+      ballPosition[0] < currentPosition[0] + brickWidth &&
+      ballPosition[1] > currentPosition[1] &&
+      ballPosition[1] < currentPosition[1] + brickHeight
+    ) {
+      console.log(ballXDirection, ballYDirection);
+
+      changeDirection();
+    }
+    // If ball hits a brick from the bricks array:
+    for (let i = 0; i < bricks.length; i++) {
+      if (
+        ballPosition[0] > bricks[i].bottomLeft[0] &&
+        ballPosition[0] < bricks[i].bottomRight[0] &&
+        ballPosition[1] + ballSize > bricks[i].bottomLeft[1] &&
+        ballPosition[1] < bricks[i].topLeft[1]
+      ) {
+        const allBricks = Array.from(document.querySelectorAll(".brick"));
+        allBricks[i].classList.remove("brick");
+        bricks.splice(i, 1);
+        changeDirection();
+      }
+    }
+
+    // If ball reaches bottom of grid i.e. game over:
+    if (ballPosition[1] <= 0) {
+      clearInterval(ballMovementInterval);
+      document.removeEventListener("keydown", arrowControls);
+      startButton.style.display = "block";
+      startButton.innerHTML = "Play Again";
+      ball.classList.remove("ball");
+    }
+  }
 }
 
 // Arrow keys to control user platform:
@@ -51,23 +124,6 @@ function changeDirection() {
   }
   if (ballXDirection === -0.5 && ballYDirection === -0.5) {
     ballYDirection = 0.5;
-    return;
-  }
-  // test:
-  if (ballXDirection === 0.9 && ballYDirection === 0.9) {
-    ballYDirection = -0.9;
-    return;
-  }
-  if (ballXDirection === -0.9 && ballYDirection === 0.9) {
-    ballXDirection = 0.9;
-    return;
-  }
-  if (ballXDirection === 0.9 && ballYDirection === -0.9) {
-    ballXDirection = -0.9;
-    return;
-  }
-  if (ballXDirection === -0.9 && ballYDirection === -0.9) {
-    ballYDirection = 0.9;
     return;
   }
 }
